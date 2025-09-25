6e4183130eaaf571ac3dcfed267e5bd71ff2ada1
sarveshpanditdev <sarveshpandit.dev@gmail.com>
2025-09-25 23:59:47 +0530

    project created

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+
+export const routes: Routes = [
+
+  {
+    path:'',
+    component: Login
+  }
+
+];
---
+++ src/app/components/home-layout/home-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-layout',
+  imports: [],
+  templateUrl: './home-layout.html',
+  styleUrl: './home-layout.css'
+})
+export class HomeLayout {
+
+}
